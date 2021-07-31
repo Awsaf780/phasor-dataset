9ff4791ab6794fcc87fc297efaa689681ce27188
Awsaf780 <awsaf780@gmail.com>
2021-07-31 23:53:48 +0600

    Add script to merge all partial datasets

---
+++ merge_dataset.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+data_folder = 'Dataset'
+cd = os.getcwd()
+directory = '{}/{}'.format(cd, data_folder)
+
+
+all_csv = os.listdir(directory)
+
+all_df = []
+for file in all_csv:
+    df = pd.read_csv('{}/{}'.format(directory, file), parse_dates=True)
+    all_df.append(df)
+
+phasor_data = pd.concat(all_df)
+
+merged_folder = 'Merged Dataset'
+
+if not os.path.exists(merged_folder):
+    os.makedirs(merged_folder)
+
+phasor_data.to_csv('{}/full_dataset.csv'.format(merged_folder), index=False)
